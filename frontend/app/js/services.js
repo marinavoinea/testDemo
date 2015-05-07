41b6f2bdd89788b535b070f96cb2e813555a769d
marinavoinea <marina.voinea@gmail.com>
2015-05-07 01:16:49 -0400

    first commit

---
+++ frontend/app/js/services.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* Services */
+
+var bookcatServices = angular.module('bookcatServices', [ 'ngResource' ]);
+
+bookcatServices.factory('Book', [ '$resource', function($resource) {
+
+	 return $resource('books/:bookCode.json', {}, { //read from json file for testing
+	//return $resource('http://localhost:8080/shopcart/store/:bookCode', {}, { //invoke REST
+		query : {
+			method : 'GET',
+			params : {
+				bookCode : 'books'
+			},
+			isArray : true
+		}
+
+	});
+} ]);
+
+// create service to provide a shopping cart that
+bookcatApp.factory("Cart", function() {
+
+	// create shopping cart
+	var myCart = new shoppingCart("BookStore");
+
+	// return data object with store and cart
+	return {
+		cart : myCart
+	};
+});
